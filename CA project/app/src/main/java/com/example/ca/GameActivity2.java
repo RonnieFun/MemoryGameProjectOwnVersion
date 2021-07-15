7dd2769f298b7b7f3feebe076ade42c8bbca338a
RonnieFun <ronyfun@hotmail.com>
2021-07-15 11:23:50 +0800

    Version 3
    Scalable UI

--- CA project/app/src/main/java/com/example/ca/GameActivity2.java
+++ CA project/app/src/main/java/com/example/ca/GameActivity2.java
@@ -3,7 +3,7 @@ package com.example.ca;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Chronometer;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -11,7 +11,6 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.squareup.picasso.Picasso;
 
@@ -19,21 +18,22 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity2 extends AppCompatActivity {
     int a = 0;
     int playerScore = 0;
+    int numberOfColumns = 3;
     int numberOfGameImages;
     ImageView firstChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gameboard);
+        setContentView(R.layout.gameboard2);
 
         //get selected images from LoadImagesActivity explicit intent
         Intent intent = getIntent();
         List<String> ChosenImagesUrls = intent.getStringArrayListExtra("SelectedImagesUrls");
-        numberOfGameImages = intent.getIntExtra("numberOfGameImages",0);
+        numberOfGameImages = intent.getIntExtra("numberOfGameImages", 0);
 
         //put urls + duplicate in new list
         List<String> GameImageUrls = new ArrayList<>();
@@ -47,14 +47,43 @@ public class GameActivity extends AppCompatActivity {
 
         //initialise scoreboard
         TextView score = findViewById(R.id.Score);
-        score.setText(getString(R.string.currentScore, 0));
+        score.setText(getString(R.string.currentScore, 0, numberOfGameImages));
 
         //load images into game
-        for (int i = 21; i <= 32; i++) {
-            String a = "imageView" + i;
-            int emptyImageId = getResources().getIdentifier(a, "id", getPackageName());
-            ImageView emptyImage = findViewById(emptyImageId);
-            loadImagesForGame(GameImageUrls.get(i - 21), emptyImage);
+        {
+            LinearLayout gameImages = findViewById(R.id.GameImages);
+
+            for (int a = 0; a < Math.ceil((double)GameImageUrls.size() / (double)numberOfColumns); a++) {
+                LinearLayout linearLayout = new LinearLayout(this);
+                linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+                LinearLayout.LayoutParams lpForRows = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                linearLayout.setLayoutParams(lpForRows);
+
+                for (int b = 0; b < numberOfColumns; b++) {
+                    ImageView imageView = new ImageView(this);
+                    LinearLayout.LayoutParams lpForImages = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT);
+                    lpForImages.weight = 1;
+                    lpForImages.height = (int) ((this.getResources().getDisplayMetrics().heightPixels) * 0.18);
+                    imageView.setLayoutParams(lpForImages);
+                    linearLayout.addView(imageView);
+                }
+                runOnUiThread(() -> gameImages.addView(linearLayout));
+            }
+
+            int i = 0;
+            int y = 0;
+            int x = 0;
+            while (i < GameImageUrls.size()) {
+                LinearLayout ll = (LinearLayout) gameImages.getChildAt(y);
+                ImageView emptyImage = (ImageView) ll.getChildAt(x);
+                loadImagesForGame(GameImageUrls.get(i), emptyImage);
+                i++;
+                x++;
+                if (x >= numberOfColumns) {
+                    y++;
+                    x = 0;
+                }
+            }
         }
     }
 
@@ -67,7 +96,7 @@ public class GameActivity extends AppCompatActivity {
 
     public void chooseImage(ImageView iv) {
         Chronometer GameTimer = findViewById(R.id.TimeElapsed);
-        ConstraintLayout images = findViewById(R.id.images);
+        LinearLayout images = findViewById(R.id.GameImages);
         TextView score = findViewById(R.id.Score);
         {
             if (firstChoice == null) {
@@ -123,42 +152,48 @@ public class GameActivity extends AppCompatActivity {
     public int scoreUpdate() {
         playerScore++;
         TextView score = findViewById(R.id.Score);
-        score.setText(getString(R.string.currentScore, playerScore));
+        score.setText(getString(R.string.currentScore, playerScore, numberOfGameImages));
 
         return playerScore;
     }
 
     //disable while showing wrong pictures
     public void disableImageSelection() {
-        ConstraintLayout images = findViewById(R.id.images);
-        for (int i = 0; i < images.getChildCount(); i++) {
-            View child = images.getChildAt(i);
-            child.setEnabled(false);
+        LinearLayout allGameImages = findViewById(R.id.GameImages);
+        for (int i = 0; i < allGameImages.getChildCount(); i++) {
+            LinearLayout imageRows = (LinearLayout) allGameImages.getChildAt(i);
+            for (int j = 0; j < imageRows.getChildCount(); j++) {
+                ImageView child = (ImageView) imageRows.getChildAt(j);
+                child.setEnabled(false);
+            }
+
         }
     }
 
     //enable after showing wrong pictures
     public void enableImageSelection(ImageView a, ImageView b) {
-        ConstraintLayout images = findViewById(R.id.images);
-        for (int i = 0; i < images.getChildCount(); i++) {
-            View child = images.getChildAt(i);
-            if (child.getForeground() != AppCompatResources.getDrawable(this, R.drawable.tick))
+        LinearLayout allGameImages = findViewById(R.id.GameImages);
+        for (int i = 0; i < allGameImages.getChildCount(); i++) {
+            LinearLayout imageRows = (LinearLayout) allGameImages.getChildAt(i);
+            for (int j = 0; j < imageRows.getChildCount(); j++) {
+                ImageView child = (ImageView) imageRows.getChildAt(j);
                 child.setEnabled(true);
+            }
+            showX(a);
+            showX(b);
         }
-        showX(a);
-        showX(b);
     }
 
     public void showCongratulations() {
         LinearLayout winGame = findViewById(R.id.WinGame);
         winGame.bringToFront();
         Chronometer countDownToMainMenu = findViewById(R.id.TimeToNewGame);
-        countDownToMainMenu.setBase(SystemClock.elapsedRealtime()+5000);
+        countDownToMainMenu.setBase(SystemClock.elapsedRealtime() + 5000);
         countDownToMainMenu.start();
     }
 
     public void startNewGame() {
-        Intent intent = new Intent(this, LoadImagesActivity.class);
+        Intent intent = new Intent(this, LoadImagesActivity2.class);
         startActivity(intent);
     }
 }
